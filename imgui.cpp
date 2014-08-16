e9b697698a95529fb9594c35c6f53429109b6dc0
Sepehr Taghdisian <sep.tagh@gmail.com>
2014-08-16 13:12:24 +0430

    fixed a typo

--- imgui.cpp
+++ imgui.cpp
@@ -5887,7 +5887,7 @@ static void StrDup_Free(char *str)
 /*
 // Copyright (c) 2004, 2005 Tristan Grimmer
 
-// Permission is hereby granted, GImGui.IO.FreeFn of charge, to any person obtaining a copy
+// Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
