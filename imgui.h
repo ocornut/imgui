cc3d41dbab026d13fb8067c9b28c542bfdf2ba53
ocornut <omarcornut@gmail.com>
2015-02-01 14:47:21 +0000

    Version number 1.31 wip

--- imgui.h
+++ imgui.h
@@ -1,4 +1,4 @@
-// ImGui library v1.30
+// ImGui library v1.31 wip
 // See .cpp file for commentary.
 // See ImGui::ShowTestWindow() for sample code.
 // Read 'Programmer guide' in .cpp for notes on how to setup ImGui in your codebase.
