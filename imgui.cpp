ocornut/imgui

commit c38c54af524280b4c7b1ddbbc8f2a6492d2c123e

Clicking on void loses keyboard-focus so application can use TAB back

imgui.cpp
@@ -2153,12 +2153,20 @@ void ImGui::Render()
             g.CurrentWindow->Visible = false;
         ImGui::End();
 
-        // Select window for move/focus when we're done with all our widgets (we use the root window ID here)
-        if (g.ActiveId == 0 && g.HoveredId == 0 && g.HoveredRootWindow != NULL && g.IO.MouseClicked[0])
+        if (g.ActiveId == 0 && g.HoveredId == 0 && g.IO.MouseClicked[0])
         {
-            IM_ASSERT(g.MovedWindow == NULL);
-            g.MovedWindow = g.HoveredWindow;
-            SetActiveId(g.HoveredRootWindow->MoveID);
+            if (g.HoveredRootWindow != NULL)
+            {
+                // Select window for move/focus when we're done with all our widgets (we use the root window ID here)
+                IM_ASSERT(g.MovedWindow == NULL);
+                g.MovedWindow = g.HoveredWindow;
+                SetActiveId(g.HoveredRootWindow->MoveID);
+            }
+            else if (g.FocusedWindow != NULL)
+            {
+                // Clicking on void disable focus
+                FocusWindow(NULL);
+            }
         }
 
         // Sort the window list so that all child windows are after their parent

commit 3c6257b8b9f8cb4494add15af0b913341053cefa

Fixed IsRootWindowOrAnyChildFocused() crashing if no window has focus (introduced yesterday in c38c54af524280b4c7b1ddbbc8f2a6492d2c123e)

imgui.cpp
@@ -3808,7 +3808,7 @@ bool ImGui::IsRootWindowOrAnyChildFocused()
 {
     ImGuiState& g = *GImGui;
     ImGuiWindow* root_window = GetCurrentWindow()->RootWindow;
-    return g.FocusedWindow->RootWindow == root_window;
+    return g.FocusedWindow && g.FocusedWindow->RootWindow == root_window;
 }
 
 float ImGui::GetWindowWidth()