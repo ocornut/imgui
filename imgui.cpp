d24474ea82e55ef4e465d613f1b2f2a004b59d0a
Branimir Karadžić <branimirkaradzic@gmail.com>
2015-04-16 14:39:04 -0700

    Fixed: warning: missing field 'w' initializer [-Wmissing-field-initializers]

--- imgui.cpp
+++ imgui.cpp
@@ -8451,7 +8451,7 @@ void ImFontAtlas::RenderCustomTexData(int pass, void* p_rects)
     ImVector<stbrp_rect>& rects = *(ImVector<stbrp_rect>*)p_rects;
     if (pass == 0)
     {
-        stbrp_rect r = { 0 };
+        stbrp_rect r = {};
         r.w = (TEX_DATA_W*2)+1;
         r.h = TEX_DATA_H+1;
         rects.push_back(r);
