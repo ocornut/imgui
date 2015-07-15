d750df918924a0a0af0546ec563e9ba2dfc57a58
ocornut <omarcornut@gmail.com>
2015-07-15 12:37:42 -0600

    Fix for Shutdown()

--- imgui.cpp
+++ imgui.cpp
@@ -9706,6 +9706,7 @@ void    ImFontAtlas::ClearInputData()
             ImGui::MemFree(ConfigData[i].FontData);
             ConfigData[i].FontData = NULL;
         }
+    ConfigData.clear();
 }
 
 void    ImFontAtlas::ClearTexData()
