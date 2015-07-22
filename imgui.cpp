4468ec001e42cb0763fc1471f0269dfb99d5db50
ocornut <omarcornut@gmail.com>
2015-07-22 06:38:19 -0600

    Fixed warning in console example (we're iterating chars)

--- imgui.cpp
+++ imgui.cpp
@@ -12919,7 +12919,7 @@ struct ExampleAppConsole
                 while (word_start > data->Buf)
                 {
                     const char c = word_start[-1];
-                    if (c == ' ' || c == '\t' || c == 0x3000 || c == ',' || c == ';')
+                    if (c == ' ' || c == '\t' || c == ',' || c == ';')
                         break;
                     word_start--;
                 }
