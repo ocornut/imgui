d7430505be84c9038aad99ffcdfcc476ca35f613
Junjie Xue <bitxue@gmail.com>
2016-08-27 19:55:15 +0800

    Update imgui_impl_glut.h

--- examples/freeglut_opengl2_example/imgui_impl_glut.h
+++ examples/freeglut_opengl2_example/imgui_impl_glut.h
@@ -1,4 +1,4 @@
-// ImGui GLFW binding with freeglut
+// ImGui freeglut binding with OpenGL
 
 // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
 // If you use this binding you'll need to call 4 functions: ImGui_ImplXXXX_Init(), ImGui_ImplXXXX_NewFrame(), ImGui::Render() and ImGui_ImplXXXX_Shutdown().
