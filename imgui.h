aa07af8743c723de444d3f0e4f85b8e21c62bb4f
Aaron <amc522@gmail.com>
2016-09-08 21:26:28 -0700

    Added option for resize grip in the lower left corner of a window.

--- imgui.h
+++ imgui.h
@@ -496,6 +496,8 @@ enum ImGuiWindowFlags_
     ImGuiWindowFlags_AlwaysVerticalScrollbar= 1 << 14,  // Always show vertical scrollbar (even if ContentSize.y < Size.y)
     ImGuiWindowFlags_AlwaysHorizontalScrollbar=1<< 15,  // Always show horizontal scrollbar (even if ContentSize.x < Size.x)
     ImGuiWindowFlags_AlwaysUseWindowPadding = 1 << 16,  // Ensure child windows without border uses style.WindowPadding (ignored by default for non-bordered child windows, because more convenient)
+    ImGuiWindowFlags_ResizeLowerLeft        = 1 << 17,  // Disable user resizing with the lower-left grip
+    ImGuiWindowFlags_ResizeLowerLeftAndRight= 1 << 18, // Disable user resizing with the lower-right grip
     // [Internal]
     ImGuiWindowFlags_ChildWindow            = 1 << 20,  // Don't use! For internal use by BeginChild()
     ImGuiWindowFlags_ChildWindowAutoFitX    = 1 << 21,  // Don't use! For internal use by BeginChild()
