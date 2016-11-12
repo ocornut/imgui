a68ac96bc4b37ce79588aec23df83ede0d2f6099
ocornut <omarcornut@gmail.com>
2016-11-12 12:48:33 +0100

    Examples: GL3: Comments about gl3w (#880)

--- examples/opengl3_example/imgui_impl_glfw_gl3.cpp
+++ examples/opengl3_example/imgui_impl_glfw_gl3.cpp
@@ -10,7 +10,7 @@
 #include "imgui_impl_glfw_gl3.h"
 
 // GL3W/GLFW
-#include <GL/gl3w.h>
+#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
 #include <GLFW/glfw3.h>
 #ifdef _WIN32
 #undef APIENTRY
