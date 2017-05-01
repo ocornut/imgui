c3d9e0a6ebfdb8c3917b4a67a561b16fece7c861
omar <omarcornut@gmail.com>
2017-05-01 15:00:42 +0200

    Examples: SDL2: Tweaked batch files for Win32.

--- examples/sdl_opengl3_example/build_win32.bat
+++ examples/sdl_opengl3_example/build_win32.bat
@@ -1,2 +1,3 @@
-set SDL2DIR=D:\T-Work\Libs\SDL\SDL2-2.0.3
-cl /Zi /MD /I ..\.. /I ..\libs\gl3w /I %SDL2DIR%\include main.cpp imgui_impl_sdl_gl3.cpp ..\..\imgui*.cpp ..\libs\gl3w\GL\gl3w.c /link /libpath:%SDL2DIR%\lib\x86 SDL2.lib SDL2main.lib opengl32.lib /subsystem:console
+@REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
+mkdir Debug
+cl /nologo /Zi /MD /I ..\.. /I ..\libs\gl3w /I %SDL_DIR%\include main.cpp imgui_impl_sdl_gl3.cpp ..\..\imgui*.cpp ..\libs\gl3w\GL\gl3w.c /FeDebug/sdl_opengl3_example.exe /FoDebug/ /link /libpath:%SDL_DIR%\lib\x86 SDL2.lib SDL2main.lib opengl32.lib /subsystem:console
