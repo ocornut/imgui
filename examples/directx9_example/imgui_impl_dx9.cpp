f9b7ecc5029d36bbc7751c93306775f996780f92
skrrtskrrt <prefaithed@gmail.com>
2017-07-07 04:55:28 -0700

    Didn't include imgui properly.
    Didn't include imgui properly.

--- examples/directx9_example/imgui_impl_dx9.cpp
+++ examples/directx9_example/imgui_impl_dx9.cpp
@@ -6,7 +6,7 @@
 // If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
 // https://github.com/ocornut/imgui
 
-#include <imgui.h>
+#include "imgui.h"
 #include "imgui_impl_dx9.h"
 
 // DirectX
