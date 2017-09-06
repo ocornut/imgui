cb2775ae54d1052a54fb6511292af6a3df245fda
omar <omarcornut@gmail.com>
2017-09-06 20:36:36 +0200

    Internals: ImGuiItemFlags comments

--- imgui_internal.h
+++ imgui_internal.h
@@ -568,8 +568,11 @@ struct ImGuiContext
 // Transient per-window flags, reset at the beginning of the frame. For child window, inherited from parent on first Begin().
 enum ImGuiItemFlags_
 {
-    ImGuiItemFlags_AllowKeyboardFocus       = 1 << 0,  // default: true
-    ImGuiItemFlags_ButtonRepeat             = 1 << 1,  // default: false    // Button() will return true multiple times based on io.KeyRepeatDelay and io.KeyRepeatRate settings.
+    ImGuiItemFlags_AllowKeyboardFocus           = 1 << 0,  // true
+    ImGuiItemFlags_ButtonRepeat                 = 1 << 1,  // false    // Button() will return true multiple times based on io.KeyRepeatDelay and io.KeyRepeatRate settings.
+    //ImGuiItemFlags_Disabled                   = 1 << 2,  // false    // All widgets appears are disabled
+    //ImGuiItemFlags_AllowNavDefaultFocus       = 1 << 3,  // true
+    //ImGuiItemFlags_SelectableDontClosePopup   = 1 << 4,  // false    // MenuItem/Selectable() automatically closes current Popup window
     ImGuiItemFlags_Default_                 = ImGuiItemFlags_AllowKeyboardFocus
 };
 
