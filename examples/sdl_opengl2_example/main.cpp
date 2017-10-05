b3099d650c2ee73683e85a0be0b6761ee741aa1d
omar <omarcornut@gmail.com>
2017-10-04 19:28:04 -0700

    Examples: Adding Roboto to the commented-out font list, and removing the unnecessary ProggyClean from there.

--- examples/sdl_opengl2_example/main.cpp
+++ examples/sdl_opengl2_example/main.cpp
@@ -36,7 +36,7 @@ int main(int, char**)
     //io.Fonts->AddFontDefault();
     //io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
     //io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
+    //io.Fonts->AddFontFromFileTTF("../../extra_fonts/Roboto-Medium.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
     //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
 
