9f200d10d569270995a8bb5893557196258fd997
omar <omarcornut@gmail.com>
2017-10-27 17:10:41 +0200

    Version 1.52

--- imgui.cpp
+++ imgui.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.52 WIP
+// dear imgui, v1.52
 // (main code and documentation)
 
 // See ImGui::ShowTestWindow() in imgui_demo.cpp for demo code.
--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.52 WIP
+// dear imgui, v1.52
 // (drawing and font code)
 
 // Contains implementation for
