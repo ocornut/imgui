4099aff8f27c263b6d614a26111b4575f4b5d0bb
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-11-03 14:40:11 +0100

    assert when using empty string as id

--- imgui.cpp
+++ imgui.cpp
@@ -1841,6 +1841,7 @@ ImGuiID ImGuiWindow::GetID(const char* str, const char* str_end)
 {
     ImGuiID seed = IDStack.back();
     ImGuiID id = ImHash(str, str_end ? (int)(str_end - str) : 0, seed);
+    IM_ASSERT(id != seed);
     ImGui::KeepAliveID(id);
     return id;
 }
