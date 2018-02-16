42c32bf00c17c230d0f264739fa37675ff1788ec
omar <omarcornut@gmail.com>
2018-02-16 22:22:10 +0100

    Examples: OpenGL2, OpenGL3: Added glPixelStorei() calls borrowed from SDL examples.

--- examples/imgui_impl_opengl2.cpp
+++ examples/imgui_impl_opengl2.cpp
@@ -141,6 +141,7 @@ bool ImGui_ImplOpenGL2_CreateFontsTexture()
     glBindTexture(GL_TEXTURE_2D, g_FontTexture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
     // Store our identifier
