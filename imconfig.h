be5d3871c01f594e6efc591369d26ee95de26b37
terryadavis <35620471+terryadavis@users.noreply.github.com>
2018-05-04 12:11:48 -0700

    Update imconfig.h

--- imconfig.h
+++ imconfig.h
@@ -52,11 +52,11 @@
 /*
 #define IM_VEC2_CLASS_EXTRA                                                 \
         ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
-        operator MyVec2() const { return MyVec2(x,y); }
+        operator MyVec2() const { return *this; }
 
 #define IM_VEC4_CLASS_EXTRA                                                 \
         ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
-        operator MyVec4() const { return MyVec4(x,y,z,w); }
+        operator MyVec4() const { return *this; }
 */
 
 //---- Use 32-bit vertex indices (default is 16-bit) to allow meshes with more than 64K vertices. Render function needs to support it.
