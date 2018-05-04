f3aa767d17c443c46bd73907db99f805922af2b1
terryadavis <35620471+terryadavis@users.noreply.github.com>
2018-05-04 12:29:55 -0700

    Update imconfig.h
    forgot its not inside of the struct

--- imconfig.h
+++ imconfig.h
@@ -50,13 +50,13 @@
 //---- Define constructor and implicit cast operators to convert back<>forth from your math types and ImVec2/ImVec4.
 // This will be inlined as part of ImVec2 and ImVec4 class declarations.
 /*
+
 #define IM_VEC2_CLASS_EXTRA                                                 \
         ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
-        operator MyVec2() const { return *this; }
-
+        operator MyVec2() const { return MyVec2(x,y); }
 #define IM_VEC4_CLASS_EXTRA                                                 \
         ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
-        operator MyVec4() const { return *this; }
+        operator MyVec4() const { return MyVec4(x,y,z,w); }
 */
 
 //---- Use 32-bit vertex indices (default is 16-bit) to allow meshes with more than 64K vertices. Render function needs to support it.
