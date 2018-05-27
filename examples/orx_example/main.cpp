7ee4f4bc7dd8166b43647cbcec27bd83a396210d
Denis Brachet <thegwydd@gmail.com>
2018-05-27 15:08:47 +0200

    updated sources

--- examples/orx_example/main.cpp
+++ examples/orx_example/main.cpp
@@ -7,31 +7,92 @@
 // See imgui_impl_glfw.cpp for details.
 
 #include "imgui.h"
-#include "imgui_impl_glfw_gl2.h"
+#include "imgui_impl_orx.h"
 #include <stdio.h>
-#include <GLFW/glfw3.h>
 
-static void glfw_error_callback(int error, const char* description)
-{
-    fprintf(stderr, "Error %d: %s\n", error, description);
-}
+extern "C" 
+    {
+#include <orx.h>
+    }
 
-int main(int, char**)
-{
-    // Setup window
-    glfwSetErrorCallback(glfw_error_callback);
-    if (!glfwInit())
-        return 1;
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui GLFW+OpenGL2 example", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+#ifdef UNUSED
+#elif defined(__GNUC__)
+# define UNUSED(x) UNUSED_ ## x __attribute__((unused))
+#elif defined(__LCLINT__)
+# define UNUSED(x) /*@unused@*/ x
+#elif defined(__cplusplus)
+# define UNUSED(x)
+#else
+# define UNUSED(x) x
+#endif
 
+
+
+
+orxVIEWPORT * gpstMainViewport = orxNULL;
+orxCAMERA * gpstMainCamera = orxNULL;
+
+//////////////////////////////////////////////////////////////////////////
+void ImGuiOrx_ResizeViewport()
+    {
+    orxFLOAT scr_w, scr_h;
+    orxDisplay_GetScreenSize(&scr_w, &scr_h);
+
+    orxFLOAT vwp_w, vwp_h;
+    orxViewport_GetSize(gpstMainViewport, &vwp_w, &vwp_h);
+
+    orxAABOX frustum;
+    orxCamera_GetFrustum(gpstMainCamera, &frustum);
+
+    orxVECTOR cam_pos;
+    orxCamera_GetPosition(gpstMainCamera, &cam_pos);
+    orxCamera_SetFrustum(gpstMainCamera, vwp_w, vwp_h, frustum.vTL.fZ, frustum.vBR.fZ);
+    orxCamera_SetPosition(gpstMainCamera, &cam_pos);
+
+    orxDEBUG_PRINT(orxDEBUG_LEVEL_LOG, "Viewport Size : %f, %f", vwp_w, vwp_h);
+    }
+
+
+//////////////////////////////////////////////////////////////////////////
+orxSTATUS orxFASTCALL ImGuiOrx_EventHandler(const orxEVENT *_pstEvent) 
+    {
+    switch (_pstEvent->eType)
+        {
+        case orxEVENT_TYPE_RENDER:
+            {
+            /* we render ImGui stuff on the top so we call it at the end of Orx frame rendering cycle */
+            if (_pstEvent->eID == orxRENDER_EVENT_STOP)
+                ImGui_Orx_Render(NULL, ImGui::GetDrawData());
+            }
+            break;
+
+        case orxEVENT_TYPE_DISPLAY:
+            ImGuiOrx_ResizeViewport();
+            break;
+
+        default:
+            break;
+        }
+ 
+    return orxSTATUS_SUCCESS;
+    }
+
+//////////////////////////////////////////////////////////////////////////
+orxSTATUS orxFASTCALL ImGuiOrx_Init()
+    {
     // Setup Dear ImGui binding
     IMGUI_CHECKVERSION();
+
+    /* create the gui context */
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    ImGui_ImplGlfwGL2_Init(window, true);
+
+    /* initialize the gui port */
+    ImGui_Orx_Init();
+
+    /* add events to manage */
+    orxEvent_AddHandler(orxEVENT_TYPE_RENDER, ImGuiOrx_EventHandler);
+    orxEvent_AddHandler(orxEVENT_TYPE_VIEWPORT, ImGuiOrx_EventHandler);
+    orxEvent_AddHandler(orxEVENT_TYPE_DISPLAY, ImGuiOrx_EventHandler);
 
     // Setup style
     ImGui::StyleColorsDark();
@@ -52,75 +113,94 @@ int main(int, char**)
     //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
     //IM_ASSERT(font != NULL);
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Creates main viewport
+    gpstMainViewport = orxViewport_CreateFromConfig("MainViewport");
 
-    // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
-        ImGui_ImplGlfwGL2_NewFrame();
-
-        // 1. Show a simple window.
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-            ImGui::Text("Hello, world!");                           // Display some text (you can use a format string too)
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our windows open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (NB: most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-        }
-
-        // 2. Show another simple window. In most cases you will use an explicit Begin/End pair to name your windows.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
-
-        // 3. Show the ImGui demo window. Most of the sample code is in ImGui::ShowDemoWindow(). Read its code to learn more about Dear ImGui!
-        if (show_demo_window)
-        {
-            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver); // Normally user code doesn't need/want to call this because positions are saved in .ini file anyway. Here we just want to make the demo initial state a bit more friendly!
-            ImGui::ShowDemoWindow(&show_demo_window);
-        }
-
-        // Rendering
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
-        ImGui::Render();
-        ImGui_ImplGlfwGL2_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
+    // Gets main camera
+    gpstMainCamera = orxViewport_GetCamera(gpstMainViewport);
+            
+    return orxSTATUS_SUCCESS;
     }
 
-    // Cleanup
-    ImGui_ImplGlfwGL2_Shutdown();
-    ImGui::DestroyContext();
+//////////////////////////////////////////////////////////////////////////
+orxSTATUS orxFASTCALL ImGuiOrx_Run()
+    {
+    ImGui_Orx_NewFrame();
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    ImVec4 clear_color = ImColor(114, 144, 154);
 
-    return 0;
-}
+    bool show_demo_window = true;
+    bool show_another_window = false;
+
+    orxFLOAT viewport_w, viewport_h;
+    orxViewport_GetSize(gpstMainViewport, &viewport_w, &viewport_h);
+
+    // 1. Show a simple window
+    // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+
+    ImGui::Text("Hello, world!");
+    ImGui::SetNextWindowSize(ImVec2(200, 500), ImGuiSetCond_FirstUseEver);
+    static float f = 0.0f;
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    ImGui::ColorEdit3("clear color", (float*)&clear_color);
+    if (ImGui::Button("Test Window")) show_demo_window ^= 1;
+    if (ImGui::Button("Another Window")) show_another_window ^= 1;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+    // 2. Show another simple window, this time using an explicit Begin/End pair
+    if (show_another_window)
+        {
+        ImGui::SetNextWindowPos(ImVec2());
+        ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
+        ImGui::Begin("Another Window", &show_another_window);
+        ImGui::Text("Hello");
+        ImGui::End();
+        }
+
+    // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+    if (show_demo_window)
+        {
+        ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+        ImGui::ShowTestWindow();
+        }
+
+    ImGui::Render();
+    return orxSTATUS_SUCCESS;
+    }
+
+//////////////////////////////////////////////////////////////////////////
+void  orxFASTCALL ImGuiOrx_Exit()
+    {
+    ImGui_Orx_Shutdown();
+    }
+
+
+/*
+#ifndef __orxMSVC__
+*/
+//////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+    {
+    /* Inits and executes orx */
+    orx_Execute(argc, argv, ImGuiOrx_Init, ImGuiOrx_Run, ImGuiOrx_Exit);
+    return EXIT_SUCCESS;
+    }
+/*
+#else  // __orxMSVC__
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+    {
+    UNUSED(hInstance);
+    UNUSED(hPrevInstance);
+    UNUSED(lpCmdLine);
+    UNUSED(nCmdShow);
+
+    / * Inits and executes orx * /
+    orx_WinExecute(ImGuiOrx_Init, ImGuiOrx_Run, ImGuiOrx_Exit);
+
+    // Done!
+    return EXIT_SUCCESS;
+    }
+#endif // __orxMSVC__
+*/
+
+
