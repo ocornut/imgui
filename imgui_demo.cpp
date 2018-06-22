00418d13e369bf53cc4b8f817eb10b8ce65f0904
omar <omarcornut@gmail.com>
2018-06-22 17:45:03 +0200

    Demo: Fixed mismatched TreePush/TreePop in Property Editor, due to incorrect revert in 826d77185e5967b5d572304945e37102efb58181 (#1895)

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -3220,7 +3220,7 @@ static void ShowExampleAppPropertyEditor(bool* p_open)
                     {
                         // Here we use a TreeNode to highlight on hover (we could use e.g. Selectable as well)
                         ImGui::AlignTextToFramePadding();
-                        ImGui::TreeNodeEx("Field", ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_Bullet, "Field_%d", i);
+                        ImGui::TreeNodeEx("Field", ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen | ImGuiTreeNodeFlags_Bullet, "Field_%d", i);
                         ImGui::NextColumn();
                         ImGui::PushItemWidth(-1);
                         if (i >= 5)
