33d92f9240af0961f04f713389cb58a00ebe0d82
Rokas Kupstys <rokups@zoho.com>
2018-07-17 17:23:40 +0300

    Fix warning when IMGUI_DEFINE_MATH_OPERATORS is already defined by build system.

--- imgui.cpp
+++ imgui.cpp
@@ -794,7 +794,9 @@
 #endif
 
 #include "imgui.h"
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
+#endif
 #include "imgui_internal.h"
 
 #include <ctype.h>      // toupper, isprint
