498c0dcb4c2a404ce8ef7c60b3f4148875c96d2f
omar <omarcornut@gmail.com>
2018-08-03 11:40:58 +0200

    Using limits.h LLONG_MIN etc. to increase old-compiler compatibility (as ll and ull prefixes were not standard). Not tested much on old compilers, relying on Clang/GCC warnings.

--- imgui.cpp
+++ imgui.cpp
@@ -848,14 +848,14 @@
 #endif
 #endif
 
-static const ImS32  IM_S32_MIN = 0x80000000; // INT_MIN;
-static const ImS32  IM_S32_MAX = 0x7FFFFFFF; // INT_MAX;
+static const ImS32  IM_S32_MIN = INT_MIN;    // (-2147483647 - 1), (0x80000000);
+static const ImS32  IM_S32_MAX = INT_MAX;    // (2147483647), (0x7FFFFFFF)
 static const ImU32  IM_U32_MIN = 0;
-static const ImU32  IM_U32_MAX = 0xFFFFFFFF;
-static const ImS64  IM_S64_MIN = -9223372036854775807ll - 1ll;
-static const ImS64  IM_S64_MAX = 9223372036854775807ll;
+static const ImU32  IM_U32_MAX = UINT_MAX;   // (0xFFFFFFFF)
+static const ImS64  IM_S64_MIN = LLONG_MIN;  // (-9223372036854775807ll - 1ll);
+static const ImS64  IM_S64_MAX = LLONG_MAX;  // (9223372036854775807ll);
 static const ImU64  IM_U64_MIN = 0;
-static const ImU64  IM_U64_MAX = 0xFFFFFFFFFFFFFFFFull;
+static const ImU64  IM_U64_MAX = ULLONG_MAX; // (0xFFFFFFFFFFFFFFFFull);
 
 // When using CTRL+TAB (or Gamepad Square+L/R) we delay the visual a little in order to reduce visual noise doing a fast switch.
 static const float NAV_WINDOWING_HIGHLIGHT_DELAY   = 0.20f; // Time before the highlight and screen dimming starts fading in
