e312363007cefa6140ca884515d2e0c6442dcb47
omar <omarcornut@gmail.com>
2018-08-28 21:59:14 +0200

    Refactor: Added empty imgui_widgets.cpp + updated project files / makefiles etc. accordingly (#2036)

---
+++ imgui_widgets.cpp
@@ -0,0 +1,25 @@
+// dear imgui, v1.64 WIP
+// (widgets code)
+
+#if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include "imgui.h"
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
+#include "imgui_internal.h"
+
+//-------------------------------------------------------------------------
+// Forward Declarations
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+// Shared Utilities
+//-------------------------------------------------------------------------
+
+//-------------------------------------------------------------------------
+// Widgets
+//-------------------------------------------------------------------------
+
