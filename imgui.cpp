ocornut/imgui

commit d9a84de9d9c28751bb8ea0ed24e4f3f1cc0e711f

Contents size is preserved while a window collapsed. Fix auto-resizing window losing their size for one frame when uncollapsed.

imgui.cpp
@@ -4332,6 +4332,9 @@ static ImVec2 CalcSizeAfterConstraint(ImGuiWindow* window, ImVec2 new_size)
 
 static ImVec2 CalcSizeContents(ImGuiWindow* window)
 {
+    if (window->Collapsed)
+        return window->SizeContents;
+
     ImVec2 sz;
     sz.x = (float)(int)((window->SizeContentsExplicit.x != 0.0f) ? window->SizeContentsExplicit.x : (window->DC.CursorMaxPos.x - window->Pos.x + window->Scroll.x));
     sz.y = (float)(int)((window->SizeContentsExplicit.y != 0.0f) ? window->SizeContentsExplicit.y : (window->DC.CursorMaxPos.y - window->Pos.y + window->Scroll.y));

commit 0cf2c05925dd7f1e908ba7ae2dcb8decc87f6fe5

Fixed auto-resize collapse bug introduced by d9a84de

imgui.cpp
@@ -4557,7 +4557,7 @@ static ImVec2 CalcSizeAfterConstraint(ImGuiWindow* window, ImVec2 new_size)
 
 static ImVec2 CalcSizeContents(ImGuiWindow* window)
 {
-    if (window->Collapsed)
+    if (window->Collapsed && (window->Flags & ImGuiWindowFlags_AlwaysAutoResize) == 0)
         return window->SizeContents;
     if (window->Hidden && window->HiddenFramesForResize == 0 && window->HiddenFramesRegular > 0)
         return window->SizeContents;