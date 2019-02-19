3c15dffc944419eb4bb17984548468270ca90486
omar <omarcornut@gmail.com>
2019-02-19 12:17:28 +0100

    Version 1.68

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.68 WIP
+// dear imgui, v1.68
 // (demo code)
 
 // Message to the person tempted to delete this file when integrating Dear ImGui into their code base:
--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.68 WIP
+// dear imgui, v1.68
 // (drawing and font code)
 
 /*
