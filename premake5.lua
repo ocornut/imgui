59f99edb5640608bca2323c22b230d9ac3d17c1a
N00TN00T <charlie.challe@live.se>
2019-03-10 21:26:14 +0100

    Added the openl3 and glfw example files to be built and set up dll exporting for windows

--- premake5.lua
+++ premake5.lua
@@ -18,13 +18,47 @@ project "imgui"
     "imstb_rectpack.h",
     "imstb_textedit.h",
     "imstb_truetype.h",
-    "imgui_demo.cpp"
+    "imgui_demo.cpp",
+    "imgui_impl_opengl3.h",
+    "imgui_impl_opengl3.cpp",
+    "imgui_impl_glfw.h",
+    "imgui_impl_glfw.cpp"
   }
+  
+  includedirs 
+  { 
+    "../imgui",
+    "../glfw/include",
+    "../glad/include"
+  }
+  
+  defines "IMGUI_IMPL_OPENGL_LOADER_GLAD"
   
 	filter "system:windows"
     systemversion "latest"
     cppdialect "C++17"
     staticruntime "On"
+    
+    defines "IM_PLATFORM_WINDOWS"
 
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
+        
+ filter "configurations:Debug*"
+    defines "IM_CONFIG_DEBUG"
+    runtime "Debug"
+    symbols "On"
+
+  filter "configurations:Release*"
+    defines "IM_CONFIG_RELEASE"
+    runtime "Release"
+    optimize "On"
+
+  filter "configurations:Dist*"
+    defines 
+    {
+      "IM_CONFIG_DIST",
+      "IM_DISABLE_ASSERTS"
+    }
+    runtime "Release"
+    optimize "On"
