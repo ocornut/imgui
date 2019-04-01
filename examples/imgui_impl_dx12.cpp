bf6185cd9877fc2152379bd755e1a15b166c9887
Chris Savoie <chris@chrissavoie.com>
2019-03-31 21:14:57 -0700

    Rendering: Add a callback for customizing how textures are set during rendering. DX12: Only generate the Font texture if it doesn't already exist. Only set texture if it actually changes. These changes can enable better integration to existing rendereres by ensuring that all ImTextureID objects are the same type as defined by the user.

--- examples/imgui_impl_dx12.cpp
+++ examples/imgui_impl_dx12.cpp
@@ -42,6 +42,7 @@ static DXGI_FORMAT                  g_RTVFormat = DXGI_FORMAT_UNKNOWN;
 static ID3D12Resource*              g_pFontTextureResource = NULL;
 static D3D12_CPU_DESCRIPTOR_HANDLE  g_hFontSrvCpuDescHandle = {};
 static D3D12_GPU_DESCRIPTOR_HANDLE  g_hFontSrvGpuDescHandle = {};
+static ImTextureID                  g_hLastTextureSet = NULL;
 
 struct FrameResources
 {
@@ -112,6 +113,9 @@ static void ImGui_ImplDX12_SetupRenderState(ImDrawData* draw_data, ID3D12Graphic
     // Setup blend factor
     const float blend_factor[4] = { 0.f, 0.f, 0.f, 0.f };
     ctx->OMSetBlendFactor(blend_factor);
+
+    // Clear texture cash to force re-applying texture
+    g_hLastTextureSet = NULL;
 }
 
 // Render function
@@ -203,6 +207,7 @@ void ImGui_ImplDX12_RenderDrawData(ImDrawData* draw_data, ID3D12GraphicsCommandL
     int vtx_offset = 0;
     int idx_offset = 0;
     ImVec2 clip_off = draw_data->DisplayPos;
+    ImGuiIO& io = ImGui::GetIO();
     for (int n = 0; n < draw_data->CmdListsCount; n++)
     {
         const ImDrawList* cmd_list = draw_data->CmdLists[n];
@@ -220,9 +225,18 @@ void ImGui_ImplDX12_RenderDrawData(ImDrawData* draw_data, ID3D12GraphicsCommandL
             }
             else
             {
+                // Set texture if it changed
+                if (pcmd->TextureId && pcmd->TextureId != g_hLastTextureSet)
+                {
+                    g_hLastTextureSet = pcmd->TextureId;
+                    if (io.SetTextureFn)
+                        io.SetTextureFn(pcmd->TextureId);
+                    else
+                        ctx->SetGraphicsRootDescriptorTable(1, *(D3D12_GPU_DESCRIPTOR_HANDLE*)&pcmd->TextureId);
+                }
+
                 // Apply Scissor, Bind texture, Draw
                 const D3D12_RECT r = { (LONG)(pcmd->ClipRect.x - clip_off.x), (LONG)(pcmd->ClipRect.y - clip_off.y), (LONG)(pcmd->ClipRect.z - clip_off.x), (LONG)(pcmd->ClipRect.w - clip_off.y) };
-                ctx->SetGraphicsRootDescriptorTable(1, *(D3D12_GPU_DESCRIPTOR_HANDLE*)&pcmd->TextureId);
                 ctx->RSSetScissorRects(1, &r);
                 ctx->DrawIndexedInstanced(pcmd->ElemCount, 1, idx_offset, vtx_offset, 0);
             }
@@ -234,8 +248,14 @@ void ImGui_ImplDX12_RenderDrawData(ImDrawData* draw_data, ID3D12GraphicsCommandL
 
 static void ImGui_ImplDX12_CreateFontsTexture()
 {
-    // Build texture atlas
+    // Build texture atlas if Font texture hasn't been set yet
     ImGuiIO& io = ImGui::GetIO();
+
+    if (io.Fonts->TexID != NULL)
+    {
+        return;
+    }
+
     unsigned char* pixels;
     int width, height;
     io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
@@ -597,6 +617,11 @@ void    ImGui_ImplDX12_InvalidateDeviceObjects()
         if (fr->IndexBuffer)  { fr->IndexBuffer->Release();  fr->IndexBuffer = NULL; }
         if (fr->VertexBuffer) { fr->VertexBuffer->Release(); fr->VertexBuffer = NULL; }
     }
+}
+
+bool ImGui_ImplDX12_Init(ID3D12Device* device, int num_frames_in_flight, DXGI_FORMAT rtv_format)
+{
+    return ImGui_ImplDX12_Init(device, num_frames_in_flight, rtv_format, D3D12_CPU_DESCRIPTOR_HANDLE(), D3D12_GPU_DESCRIPTOR_HANDLE());
 }
 
 bool ImGui_ImplDX12_Init(ID3D12Device* device, int num_frames_in_flight, DXGI_FORMAT rtv_format,
