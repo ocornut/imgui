d1d5075b66f93686954154ceaba7fe30abc41f4b
omar <omarcornut@gmail.com>
2019-05-06 14:17:39 +0200

    Version 1.70

--- imgui.cpp
+++ imgui.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.70 WIP
+// dear imgui, v1.70
 // (main code and documentation)
 
 // Call and read ImGui::ShowDemoWindow() in imgui_demo.cpp for demo code.
--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.70 WIP
+// dear imgui, v1.70
 // (demo code)
 
 // Message to the person tempted to delete this file when integrating Dear ImGui into their code base:
