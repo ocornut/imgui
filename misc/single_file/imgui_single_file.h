f03c00bc89c4ba9b45f4e216d943e1d890dfbac0
Rokas Kupstys <rokups@zoho.com>
2020-01-10 19:36:26 +0200

    Added imgui_single_file.h, We use this to validate compiling all *.cpp in same compilation unit.
    Removed Unity builds stuff from example_null/. CI builds a temporary .cpp file.

---
+++ misc/single_file/imgui_single_file.h
@@ -0,0 +1,17 @@
+// imgui_single_file.h
+// We use this to validate compiling all *.cpp files in a same compilation unit.
+// Users of that technique (also called "Unity builds") can generally provide this themselves,
+// so we don't really recommend you use this in your projects.
+
+// Do this:
+//    #define IMGUI_IMPLEMENTATION
+// Before you include this file in *one* C++ file to create the implementation.
+// Using this in your project will leak the contents of imgui_internal.h and ImVec2 operators in this compilation unit.
+#include "../../imgui.h"
+
+#ifdef IMGUI_IMPLEMENTATION
+#include "../../imgui.cpp"
+#include "../../imgui_demo.cpp"
+#include "../../imgui_draw.cpp"
+#include "../../imgui_widgets.cpp"
+#endif
