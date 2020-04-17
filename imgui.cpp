c958df1838fc6ed26290e59b05edd0c753fa8f8c
geneotech <patryk.czachurski@gmail.com>
2020-04-17 21:18:27 +0200

    Use IM_UNUSED instead of (void) for the backup_dock_id

--- imgui.cpp
+++ imgui.cpp
@@ -14221,7 +14221,8 @@ void ImGui::DockBuilderRemoveNodeDockedWindows(ImGuiID root_id, bool clear_persi
         bool want_removal = (root_id == 0) || (window->DockNode && DockNodeGetRootNode(window->DockNode)->ID == root_id) || (window->DockNodeAsHost && window->DockNodeAsHost->ID == root_id);
         if (want_removal)
         {
-            const ImGuiID backup_dock_id = window->DockId; (void)backup_dock_id;
+            const ImGuiID backup_dock_id = window->DockId;
+            IM_UNUSED(backup_dock_id);
             DockContextProcessUndockWindow(ctx, window, clear_persistent_docking_references);
             if (!clear_persistent_docking_references)
                 IM_ASSERT(window->DockId == backup_dock_id);
