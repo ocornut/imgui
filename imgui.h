e842d070daf800b545fae5d305c1e7ed1a90ab57
Silent <CookiePLMonster@users.noreply.github.com>
2020-04-19 14:40:08 +0200

    Fix wrong comment in ImGuiCond_

--- imgui.h
+++ imgui.h
@@ -1265,7 +1265,7 @@ enum ImGuiMouseCursor_
 enum ImGuiCond_
 {
     ImGuiCond_Always        = 1 << 0,   // Set the variable
-    ImGuiCond_Once          = 1 << 1,   // Set the variable once per runtime session (only the first call with succeed)
+    ImGuiCond_Once          = 1 << 1,   // Set the variable once per runtime session (only the first call will succeed)
     ImGuiCond_FirstUseEver  = 1 << 2,   // Set the variable if the object/window has no persistently saved data (no entry in .ini file)
     ImGuiCond_Appearing     = 1 << 3    // Set the variable if the object/window is appearing after being hidden/inactive (or the first time)
 };
