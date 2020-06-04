4e9016440ff1b18f008e5f8d364eda8c73894e31
Leonixidas <leanderdehertogh@hotmail.com>
2020-06-04 14:33:13 +0200

    Adding premake file to docking branch of ImGui

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+project "Imgui"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetpye.h",
+        "imgui_demo.cpp"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "c++17"
+        staticruntime "On"
+
+    
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+        buildoptions "/MTd"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+        buildoptions "/MT"
