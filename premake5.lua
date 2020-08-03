a98709358090eb49a47df2b7dfdc248b97752d94
vincentlorant <lorant.vincents@gmail.com>
2020-08-03 10:57:43 +0900

    add premake and bin

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
