02e37991ae25f289d3407e8d37cdd6a3083fdb04
Rory O'Connell <19547+RoryO@users.noreply.github.com>
2020-08-05 19:01:35 -0700

    Fix updating clear color

--- examples/example_glfw_vulkan/main.cpp
+++ examples/example_glfw_vulkan/main.cpp
@@ -456,6 +456,10 @@ int main(int, char**)
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    wd->ClearValue.color.float32[0] = clear_color.x;
+    wd->ClearValue.color.float32[1] = clear_color.y;
+    wd->ClearValue.color.float32[2] = clear_color.z;
+    wd->ClearValue.color.float32[3] = clear_color.w;
 
     // Main loop
     while (!glfwWindowShouldClose(g_Window))
@@ -497,7 +501,13 @@ int main(int, char**)
             ImGui::Checkbox("Another Window", &show_another_window);
 
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+            if(ImGui::ColorEdit3("clear color", (float*)&clear_color)) // Edit 3 floats representing a color
+            {
+                wd->ClearValue.color.float32[0] = clear_color.x;
+                wd->ClearValue.color.float32[1] = clear_color.y;
+                wd->ClearValue.color.float32[2] = clear_color.z;
+                wd->ClearValue.color.float32[3] = clear_color.w;
+            };
 
             if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
                 counter++;
