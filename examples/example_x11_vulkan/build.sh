575f24445c3c2830c202046d676847f4bcd2f23b
Rory OConnell <19547+RoryO@users.noreply.github.com>
2020-08-09 13:35:23 -0700

    Add xcb-xkb to linker

--- examples/example_x11_vulkan/build.sh
+++ examples/example_x11_vulkan/build.sh
@@ -6,5 +6,5 @@ if ! test -d Release; then
     mkdir Release
 fi
 
-cc -std=c++17 -g -I .. -I ../.. -o Debug/example_x11_vulkan main.cpp ../imgui_impl_x11.cpp ../imgui_impl_vulkan.cpp ../../imgui*.cpp -lm -lxcb -lxcb-keysyms -lxcb-xfixes -lstdc++ -lvulkan -lrt
-cc -std=c++17 -O3 -I .. -I ../.. -o Release/example_x11_vulkan main.cpp ../imgui_impl_x11.cpp ../imgui_impl_vulkan.cpp ../../imgui*.cpp -lm -lxcb -lxcb-keysyms -lxcb-xfixes -lstdc++ -lvulkan -lrt
+cc -std=c++17 -g -I .. -I ../.. -o Debug/example_x11_vulkan main.cpp ../imgui_impl_x11.cpp ../imgui_impl_vulkan.cpp ../../imgui*.cpp -lm -lxcb -lxcb-keysyms -lxcb-xfixes -lxcb-xkb -lxcb-cursor -lstdc++ -lvulkan -lrt
+cc -std=c++17 -O3 -I .. -I ../.. -o Release/example_x11_vulkan main.cpp ../imgui_impl_x11.cpp ../imgui_impl_vulkan.cpp ../../imgui*.cpp -lm -lxcb -lxcb-keysyms -lxcb-xfixes -lxcb-xkb -lxcb-cursor -lstdc++ -lvulkan -lrt
