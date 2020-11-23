2afdfa602fb0dd5ff1502910fede80b8779ebc2d
vaiorabbit <vaiorabbit>
2020-11-23 17:19:56 +0900

    Rebuild ImFontAtlas::GetGlyphRangesJapanese offset table (#3627)
    - GetGlyphRangesJapanese now supports
      - 2136 'Joyo (meaning "for regular use" or "for common use")' Kanji
      - 863 'Jinmeiyo" (meaning "for personal name")' Kanji

--- imgui.h
+++ imgui.h
@@ -2343,7 +2343,7 @@ struct ImFontAtlas
     // NB: Consider using ImFontGlyphRangesBuilder to build glyph ranges from textual data.
     IMGUI_API const ImWchar*    GetGlyphRangesDefault();                // Basic Latin, Extended Latin
     IMGUI_API const ImWchar*    GetGlyphRangesKorean();                 // Default + Korean characters
-    IMGUI_API const ImWchar*    GetGlyphRangesJapanese();               // Default + Hiragana, Katakana, Half-Width, Selection of 1946 Ideographs
+    IMGUI_API const ImWchar*    GetGlyphRangesJapanese();               // Default + Hiragana, Katakana, Half-Width, Selection of 2999 Ideographs
     IMGUI_API const ImWchar*    GetGlyphRangesChineseFull();            // Default + Half-Width + Japanese Hiragana/Katakana + full set of about 21000 CJK Unified Ideographs
     IMGUI_API const ImWchar*    GetGlyphRangesChineseSimplifiedCommon();// Default + Half-Width + Japanese Hiragana/Katakana + set of 2500 CJK Unified Ideographs for common simplified Chinese
     IMGUI_API const ImWchar*    GetGlyphRangesCyrillic();               // Default + about 400 Cyrillic characters
