9fcc800a232c1c97c898fc7cad218661a95f6001
ocornut <omarcornut@gmail.com>
2020-11-26 22:17:55 +0100

    ImStrv: rework toward ensuring End is always set to constant can be compile time calculated

--- imgui.cpp
+++ imgui.cpp
@@ -1637,7 +1637,6 @@ const char* ImStristr(const char* haystack, const char* haystack_end, const char
 
 const char* ImStrstr(ImStrv haystack, ImStrv needle)
 {
-    IM_IMSTR_ENSURE_HAS_END(needle);
     const char un0 = (char)*needle.Begin;
     while ((!haystack.End && *haystack.Begin) || (haystack.End && haystack.Begin < haystack.End))
     {
@@ -2367,8 +2366,7 @@ bool ImGuiTextFilter::PassFilter(ImStrv text) const
     if (Filters.empty())
         return true;
 
-    IM_IMSTR_ENSURE_HAS_END(text);
-    if (text.Empty())
+    if (text.Empty()) // FIXME-IMSTR
         text.Begin = text.End = "";
 
     for (int i = 0; i != Filters.Size; i++)
@@ -3038,14 +3036,9 @@ void ImGui::RenderText(ImVec2 pos, ImStrv text, bool hide_text_after_hash)
     // Hide anything after a '##' string
     const char* text_display_end;
     if (hide_text_after_hash)
-    {
         text_display_end = FindRenderedTextEnd(text);
-    }
     else
-    {
-        IM_IMSTR_ENSURE_HAS_END(text);
         text_display_end = text.End;
-    }
 
     if (text.Begin != text_display_end)
     {
@@ -3059,7 +3052,6 @@ void ImGui::RenderTextWrapped(ImVec2 pos, ImStrv text, float wrap_width)
 {
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
-    IM_IMSTR_ENSURE_HAS_END(text);
 
     if (text.Begin != text.End)
     {
@@ -3318,7 +3310,6 @@ ImGuiID ImGuiWindow::GetID(ImStrv str)
     ImGuiID seed = IDStack.back();
     ImGuiID id = ImHashStr(str, seed);
     ImGuiContext& g = *GImGui;
-    IM_IMSTR_ENSURE_HAS_END(str);
     if (g.DebugHookIdInfo == id)
         ImGui::DebugHookIdInfo(id, ImGuiDataType_String, str.Begin, str.End);
     return id;
@@ -4981,7 +4972,6 @@ ImVec2 ImGui::CalcTextSize(ImStrv text, bool hide_text_after_double_hash, float 
 {
     ImGuiContext& g = *GImGui;
 
-    IM_IMSTR_ENSURE_HAS_END(text);
     if (hide_text_after_double_hash)
         text.End = FindRenderedTextEnd(text);      // Hide anything after a '##' string
 
