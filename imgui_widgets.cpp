9974bf66d6f284adcb353f3b8ec7b91b58b9f7fc
Sammy Fatnassi <sammyfreg@gmail.com>
2021-01-26 09:12:56 +0900

    Removed unecessary parameters and whitespace

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -7306,7 +7306,7 @@ static ImU32   ImGui::TabBarCalcTabID(ImGuiTabBar* tab_bar, const char* label, I
     if (docked_window != NULL)
     {
         IM_UNUSED(tab_bar);
-        IM_ASSERT(tab_bar->Flags & ImGuiTabBarFlags_DockNode);        
+        IM_ASSERT(tab_bar->Flags & ImGuiTabBarFlags_DockNode);
         ImGuiID id = ImHashStr(label);
         KeepAliveID(id);
         return id;
