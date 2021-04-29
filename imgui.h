9fa23d1473d0412be5fc1ed00ce4941f13a6a923
Weston McNamara <westonfmcnamara@gmail.com>
2021-04-29 10:33:55 -0400

    Changed memset calls

--- imgui.h
+++ imgui.h
@@ -2067,9 +2067,9 @@ struct ImGuiStorage
     {
         ImGuiID key;
         union { int val_i; float val_f; void* val_p; };
-        ImGuiStoragePair(ImGuiID _key, int _val_i)   { memset(val_p, 0, sizeof(val_p)); key = _key; val_i = _val_i; }
-        ImGuiStoragePair(ImGuiID _key, float _val_f) { memset(val_p, 0, sizeof(val_p)); key = _key; val_f = _val_f; }
-        ImGuiStoragePair(ImGuiID _key, void* _val_p) { key = _key; val_p = _val_p; }
+        ImGuiStoragePair(ImGuiID _key, int _val_i)   { memset(this, 0, sizeof(*this)); key = _key; val_i = _val_i; }
+        ImGuiStoragePair(ImGuiID _key, float _val_f) { memset(this, 0, sizeof(*this)); key = _key; val_f = _val_f; }
+        ImGuiStoragePair(ImGuiID _key, void* _val_p) { memset(this, 0, sizeof(*this)); key = _key; val_p = _val_p; }
     };
 
     ImVector<ImGuiStoragePair>      Data;
