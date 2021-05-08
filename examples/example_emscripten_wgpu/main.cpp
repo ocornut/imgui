0acb1a5b756cddb3e1aa9a3b36d3b474de54c4a1
Basil Fierz <basil.fierz@hotmail.com>
2021-05-08 18:39:30 +0200

    Update ColorPassAttachment

--- examples/example_emscripten_wgpu/main.cpp
+++ examples/example_emscripten_wgpu/main.cpp
@@ -202,11 +202,11 @@ static void main_loop(void* window)
     // Rendering
     ImGui::Render();
 
-    WGPURenderPassColorAttachmentDescriptor color_attachments = {};
+    WGPURenderPassColorAttachment color_attachments = {};
     color_attachments.loadOp = WGPULoadOp_Clear;
     color_attachments.storeOp = WGPUStoreOp_Store;
     color_attachments.clearColor = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
-    color_attachments.attachment = wgpuSwapChainGetCurrentTextureView(wgpu_swap_chain);
+    color_attachments.view = wgpuSwapChainGetCurrentTextureView(wgpu_swap_chain);
     WGPURenderPassDescriptor render_pass_desc = {};
     render_pass_desc.colorAttachmentCount = 1;
     render_pass_desc.colorAttachments = &color_attachments;
