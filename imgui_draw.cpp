499a10848910a503c8adbdb55aa2c5a22493f0c3
thedmd <michcic@gmail.com>
2021-08-11 13:21:27 +0200

    AddCircle{Filled} - Use _PathArcToFastEx directly to emit circle vertices for 12 segment case (#4419)
    Before introduction of adaptive arcs PathArcToFast() were operating on 12 vertices table.
    Now it is replaced by internal _PathArcToFastEx() doing same job but with greater granularity,
    PathArcToFast was left as a fallback.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -1494,7 +1494,7 @@ void ImDrawList::AddCircle(const ImVec2& center, float radius, ImU32 col, int nu
     // Because we are filling a closed shape we remove 1 from the count of segments/points
     const float a_max = (IM_PI * 2.0f) * ((float)num_segments - 1.0f) / (float)num_segments;
     if (num_segments == 12)
-        PathArcToFast(center, radius - 0.5f, 0, 12 - 1);
+        _PathArcToFastEx(center, radius - 0.5f, 0, IM_DRAWLIST_ARCFAST_SAMPLE_MAX - 1, 0);
     else
         PathArcTo(center, radius - 0.5f, 0.0f, a_max, num_segments - 1);
     PathStroke(col, ImDrawFlags_Closed, thickness);
@@ -1520,7 +1520,7 @@ void ImDrawList::AddCircleFilled(const ImVec2& center, float radius, ImU32 col, 
     // Because we are filling a closed shape we remove 1 from the count of segments/points
     const float a_max = (IM_PI * 2.0f) * ((float)num_segments - 1.0f) / (float)num_segments;
     if (num_segments == 12)
-        PathArcToFast(center, radius, 0, 12 - 1);
+        _PathArcToFastEx(center, radius, 0, IM_DRAWLIST_ARCFAST_SAMPLE_MAX - 1, 0);
     else
         PathArcTo(center, radius, 0.0f, a_max, num_segments - 1);
     PathFillConvex(col);
