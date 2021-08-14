261cbfc7a15a7eed57336837d36a36731646068e
Light <VerodiciaTheNatureGoddess@gmail.com>
2021-08-14 16:25:38 +0430

    Update imconfig.h

--- imconfig.h
+++ imconfig.h
@@ -79,11 +79,13 @@
 
 //---- Define constructor and implicit cast operators to convert back<>forth between your math types and ImVec2/ImVec4.
 // This will be inlined as part of ImVec2 and ImVec4 class declarations.
-/*
 #define IM_VEC2_CLASS_EXTRA                                                 \
-        ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
-        operator MyVec2() const { return MyVec2(x,y); }
+        bool operator ==(const ImVec2& other) const { return x == other.x && y == other.y; } \
+        bool operator !=(const ImVec2& other) const { return !(*this == other); }
+//      ImVec2(const MyVec2& f) { x = f.x; y = f.y; }                       \
+//      operator MyVec2() const { return MyVec2(x,y); }
 
+/*
 #define IM_VEC4_CLASS_EXTRA                                                 \
         ImVec4(const MyVec4& f) { x = f.x; y = f.y; z = f.z; w = f.w; }     \
         operator MyVec4() const { return MyVec4(x,y,z,w); }
