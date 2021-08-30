53589092b22b6fa5e92140f30621b6d40eea4eb2
ocornut <omarcornut@gmail.com>
2021-08-30 20:53:22 +0200

    Docking: warning fix for when IM_ASSERT() is empty

--- imgui.cpp
+++ imgui.cpp
@@ -13356,7 +13356,8 @@ static void ImGui::DockNodeRemoveWindow(ImGuiDockNode* node, ImGuiWindow* window
             erased = true;
             break;
         }
-    IM_ASSERT(erased);
+    if (!erased)
+        IM_ASSERT(erased);
     if (node->VisibleWindow == window)
         node->VisibleWindow = NULL;
 
