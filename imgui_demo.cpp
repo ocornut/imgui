ac9fb9ba272f4c3d5064260c5cb9db3b43986f64
Pascal Thomet <pthomet@gmail.com>
2021-11-28 10:31:03 +0100

    Demo: Demo markers: show code location by highlight and tooltip
    ImGuiDemoMarkerCallback_Default is the default callback
    used by IMGUI_DEMO_MARKER, but this can be overriden via
    GImGuiDemoMarkerCallback
    
    ImGuiDemoMarkerHighlightZone() is able to graphically highlight
    a *hovered* section of the demo (it keeps track of the graphical
    location of each section).

--- imgui_demo.cpp
+++ imgui_demo.cpp
@@ -222,9 +222,10 @@ static void HelpMarker(const char* desc)
 
 // Helper to wire demo markers located in code to an interactive browser
 typedef void (*ImGuiDemoMarkerCallback)(const char* file, int line, const char* section, void* user_data);
+void ImGuiDemoMarkerCallback_Default(const char* file, int line, const char* section, void* user_data);
 extern ImGuiDemoMarkerCallback      GImGuiDemoMarkerCallback;
 extern void*                        GImGuiDemoMarkerCallbackUserData;
-ImGuiDemoMarkerCallback             GImGuiDemoMarkerCallback = NULL;
+ImGuiDemoMarkerCallback         GImGuiDemoMarkerCallback = ImGuiDemoMarkerCallback_Default;
 void*                               GImGuiDemoMarkerCallbackUserData = NULL;
 extern bool                     GImGuiDemoMarker_IsActive;
 bool                            GImGuiDemoMarker_IsActive = false;
@@ -8052,6 +8053,10 @@ void ShowExampleAppDocuments(bool* p_open)
 // Utilities that provide an interactive "code lookup" via the IMGUI_DEMO_MARKER macro
 //-----------------------------------------------------------------------------
 
+// Forward declarations
+bool ImGuiDemoMarkerHighlightZone(int line_number);
+
+// [sub section] ImGuiDemoMarker_GuiToggle()
 // Display a "Code Lookup" checkbox that toggles interactive code browsing
 void ImGuiDemoMarker_GuiToggle()
 {
@@ -8068,6 +8073,184 @@ void ImGuiDemoMarker_GuiToggle()
         GImGuiDemoMarker_IsActive = true;
     if (GImGuiDemoMarker_IsActive && ImGui::IsKeyPressed(ImGui::GetKeyIndex(ImGuiKey_Escape)))
         GImGuiDemoMarker_IsActive = false;
+}
+
+// [sub section] ImGuiDemoMarkerCallback_Default()
+// ImGuiDemoMarkerCallback_Default is the default callback used by IMGUI_DEMO_MARKER,
+// but this can be overriden via GImGuiDemoMarkerCallback
+void ImGuiDemoMarkerCallback_Default(const char* /*file*/, int line, const char* section, void* /*user_data*/)
+{
+    if (!GImGuiDemoMarker_IsActive)
+        return;
+    if (ImGuiDemoMarkerHighlightZone(line))
+    {
+        ImGui::SetTooltip(
+            "Code Lookup\n"
+            "IMGUI_DEMO_MARKER(\"%s\") at imgui_demo.cpp:%d\n\n"
+            "Press \"Esc\" to exit this mode",
+            section, line);
+    }
+}
+
+// [sub section] ImGuiDemoMarkerHighlightZone()
+// `bool ImGuiDemoMarkerHighlightZone(int line_number)` is able to graphically highlight a *hovered* section
+// of the demo (it keeps track of graphical location of each section).
+// Each zone is identified by its source code line number, and ImGuiDemoMarkerHighlightZone will return true if
+// it is currently highlighted.
+#include "imgui_internal.h" //  we import GetCurrentWindow() (which is defined in imgui_internal)
+namespace ImGuiDemoMarkerHighlight_Impl
+{
+    // The DemoMarkersRegistry class stores the boundings for the different calls to the IMGUI_DEMO_MARKER macro.
+    // It handles the display and handling of the "Help/Code lookup" button.
+    class DemoMarkersRegistry
+    {
+    private:
+        // A ZoneBoundings specifies a rectangular bounding for the widgets whose code is given
+        // *after* a call to IMGUI_DEMO_MARKER. This bounding will extend down to the next IMGUI_DEMO_MARKER macro call.
+        // It always occupies the full width of the current window.
+        struct ZoneBoundings
+        {
+            ZoneBoundings() : SourceLineNumber(-1), MinY(-1.0f), MaxY(-1.0f), Window(NULL) {}
+            int SourceLineNumber; // Source code location
+            float MinY, MaxY;     // Location of this zone inside its parent window
+            ImGuiWindow* Window;  // Current window when IMGUI_DEMO_MARKER was called
+        };
+
+    public:
+        DemoMarkersRegistry() :
+            AllZonesBoundings(),
+            PreviousZoneSourceLine(-1)
+        {
+        }
+
+        // Highlight starts a demo marker zone.
+        // If the highlight mode is active and the demo marker zone is hovered, it will highlight it,
+        // display a tooltip and return true. Otherwise it will return false.
+        bool Highlight(int line_number)
+        {
+            // This will store the bounding for the next widgets, and this bounding will extend until the next call to DemoMarker
+            StoreZoneBoundings(line_number);
+            ZoneBoundings& zone_boundings = GetZoneBoundingsForLine(line_number);
+
+            // Handle mouse and keyboard actions if the zone is hovered
+            bool is_mouve_hovering_zone = IsMouseHoveringZoneBoundings(zone_boundings);
+            if (! is_mouve_hovering_zone)
+                return false;
+
+            HighlightZone(zone_boundings);
+            return true;
+        }
+
+    private:
+        void StoreZoneBoundings(int line_number)
+        {
+            // Store info about marker
+            ZoneBoundings current_zone_boundings;
+            {
+                if (HasZoneBoundingsForLine(line_number))
+                    current_zone_boundings = GetZoneBoundingsForLine(line_number);
+                else
+                    current_zone_boundings.SourceLineNumber = line_number;
+            }
+
+            // Store MinY position for current marker
+            current_zone_boundings.Window = ImGui::GetCurrentWindow();
+            current_zone_boundings.MinY = ImGui::GetCursorScreenPos().y;
+
+            // Store current marker in list
+            SetZoneBoundingsForLine(line_number, current_zone_boundings);
+
+            // Store Max position for previous marker
+            if (HasZoneBoundingsForLine(PreviousZoneSourceLine))
+            {
+                ZoneBoundings& previous_zone_boundings = GetZoneBoundingsForLine(PreviousZoneSourceLine);
+                if (previous_zone_boundings.Window == ImGui::GetCurrentWindow())
+                    previous_zone_boundings.MaxY = ImGui::GetCursorScreenPos().y;
+            }
+
+            PreviousZoneSourceLine = line_number;
+        }
+
+        bool IsMouseHoveringZoneBoundings(const ZoneBoundings& zone_boundings)
+        {
+            if (!ImGui::IsWindowHovered(ImGuiHoveredFlags_AllowWhenBlockedByActiveItem | ImGuiHoveredFlags_RootAndChildWindows | ImGuiHoveredFlags_NoPopupHierarchy))
+                return false;
+            float y_mouse = ImGui::GetMousePos().y;
+            float x_mouse = ImGui::GetMousePos().x;
+            return (
+                (y_mouse >= zone_boundings.MinY)
+                && ( (y_mouse < zone_boundings.MaxY) || (zone_boundings.MaxY < 0.f) )
+                && ( (x_mouse >= ImGui::GetWindowPos().x) && ( x_mouse < ImGui::GetWindowPos().x + ImGui::GetWindowSize().x ))
+            );
+        }
+
+        void HighlightZone(const ZoneBoundings zone_boundings)
+        {
+            // tl_dim / br_dim : top_left and bottom_right corners of the dimmed zone.
+            ImVec2 tl_dim = ImGui::GetWindowPos();
+            ImVec2 br_dim(ImGui::GetWindowPos().x + ImGui::GetWindowSize().x, ImGui::GetWindowPos().y + ImGui::GetWindowSize().y);
+
+            // tl_zone / br_zone: top_left and bottom_right corner of the highlighted zone
+            float minY = zone_boundings.MinY < ImGui::GetWindowPos().y ? ImGui::GetWindowPos().y : zone_boundings.MinY;
+            ImVec2 tl_zone(ImGui::GetWindowPos().x, minY);
+            float maxY = zone_boundings.MaxY > 0.f ? zone_boundings.MaxY : ImGui::GetWindowPos().y + ImGui::GetWindowHeight();
+            ImVec2 br_zone(ImGui::GetWindowPos().x + ImGui::GetWindowWidth(), maxY);
+
+            ImDrawList* draw_list = ImGui::GetForegroundDrawList();
+            ImU32 dim_color = IM_COL32(127, 127, 127, 100);
+
+            draw_list->AddRectFilled(tl_dim, ImVec2(br_dim.x, tl_zone.y), dim_color);
+
+            draw_list->AddRectFilled(ImVec2(tl_dim.x, tl_zone.y), ImVec2(tl_zone.x, br_zone.y), dim_color);
+            draw_list->AddRectFilled(ImVec2(br_zone.x, tl_zone.y), ImVec2(br_dim.x, br_zone.y), dim_color);
+
+            draw_list->AddRectFilled(ImVec2(tl_dim.x, br_zone.y), ImVec2(br_dim.x, br_dim.y), dim_color);
+        }
+
+        bool HasZoneBoundingsForLine(int line_number)
+        {
+            for (int i = 0; i < AllZonesBoundings.size(); ++i)
+                if (AllZonesBoundings[i].SourceLineNumber == line_number)
+                    return true;
+            return false;
+        }
+
+        ZoneBoundings& GetZoneBoundingsForLine(int line_number)
+        {
+            IM_ASSERT(HasZoneBoundingsForLine(line_number)); // Please call HasZoneBoundingsForLine before!
+            for (int i = 0; i < AllZonesBoundings.size(); ++i)
+            {
+                ZoneBoundings& zone = AllZonesBoundings[i];
+                if (zone.SourceLineNumber == line_number)
+                    return zone;
+            }
+
+            IM_ASSERT(false);       // We should never get there!
+            static ZoneBoundings dummy; return dummy; // Make the compiler happy
+        }
+
+        void SetZoneBoundingsForLine(int line_number, const ZoneBoundings& zone_boundings)
+        {
+            if (HasZoneBoundingsForLine(line_number))
+            {
+                ZoneBoundings& old_boundings = GetZoneBoundingsForLine(line_number);
+                old_boundings = zone_boundings;
+            }
+            else
+            {
+                AllZonesBoundings.push_back(zone_boundings);
+            }
+        }
+
+        // Members
+        ImVector<ZoneBoundings> AllZonesBoundings;    // All boundings for all the calls to DEMO_MARKERS
+        int PreviousZoneSourceLine;                   // Location of the previous call to DEMO_MARKERS (used to end the previous bounding)
+    };
+    static DemoMarkersRegistry GDemoMarkersRegistry;  // Global instance used by the IMGUI_DEMO_MARKER macro
+} // namespace ImGuiDemoMarkerHighlight_Impl
+bool ImGuiDemoMarkerHighlightZone(int line_number)
+{
+    return ImGuiDemoMarkerHighlight_Impl::GDemoMarkersRegistry.Highlight(line_number);
 }
 
 // End of Demo code
