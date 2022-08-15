c3f7f41756b29bb8f8a13811a2f2fac3174d95c5
Tom Xiong <9060084+sliptime@users.noreply.github.com>
2022-08-15 09:20:32 -0700

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,27 @@
+project "ImGui"
+  kind "StaticLib"
+  language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+    "imgui.h",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imgui_rectpack.h",
+    "imgui_textedit.h",
+    "imgui_truetype.h",
+    "imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+    cppdialect "C++17"
+		staticruntime "On"
+
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
