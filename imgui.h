2a2af510924b67b98a849d9ba6c0ffa72a7b82f8
vjotchere <vjotchere@gmail.com>
2022-10-23 18:21:39 -0400

    Selectable: Generate press events on right click

--- imgui.h
+++ imgui.h
@@ -1070,6 +1070,7 @@ enum ImGuiSelectableFlags_
     ImGuiSelectableFlags_AllowDoubleClick   = 1 << 2,   // Generate press events on double clicks too
     ImGuiSelectableFlags_Disabled           = 1 << 3,   // Cannot be selected, display grayed out text
     ImGuiSelectableFlags_AllowItemOverlap   = 1 << 4,   // (WIP) Hit testing to allow subsequent widgets to overlap this one
+    ImGuiSelectableFlags_AllowRightClick    = 1 << 5,   // Generate press events on right clicks also
 };
 
 // Flags for ImGui::BeginCombo()
