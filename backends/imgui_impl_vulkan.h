5534f55eefd050f98052dc19190cac843210906c
Aðalsteinn Hjörleifsson <adalsteinn@adalsteinn.is>
2023-07-07 00:50:42 +0200

    changed #defines to avoid double definitions

--- backends/imgui_impl_vulkan.h
+++ backends/imgui_impl_vulkan.h
@@ -43,7 +43,7 @@
 #if defined(IMGUI_IMPL_VULKAN_NO_PROTOTYPES) && !defined(VK_NO_PROTOTYPES)
 #define VK_NO_PROTOTYPES
 #endif
-#include <vulkan/vulkan.h>
+#include <Volk/volk.h>
 
 // Initialization data, for ImGui_ImplVulkan_Init()
 // [Please zero-clear before use!]
