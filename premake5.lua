6cc26511d8182689ca37410b20ac74e9b9e80594
Kirill <vologdinkirill48@gmail.com>
2023-08-10 15:33:01 +0600

    Switch branch to docking

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+    --defines
+    --{
+    --    "IMGUI_API=__declspec(dllexport)"
+    --}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
