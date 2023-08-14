8bb1e631d85bcb7d99323c7c74d653ff9b0cbb7b
ahmed walid <ahmed.walid605@yahoo.com>
2023-08-15 00:03:20 +0200

    [UWP backend] Remove unused files in the example

--- examples/example_uwp_directx11/pch.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include <windows.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.ApplicationModel.Core.h>
-#include <winrt/Windows.Graphics.Display.h>
-#include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.UI.Input.h>
