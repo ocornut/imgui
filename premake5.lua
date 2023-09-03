a75023f6a123c7a45a9351eddaad0f68fe99cb53
Liudas Augustinas <liudasaugust@gmail.com>
2023-09-03 22:29:00 +0300

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,45 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir("%{wks.location}/Build/Binary/"..outputdir.."/%{prj.name}");
+	objdir("%{wks.location}/Build/Intermediate/"..outputdir.."/%{prj.name}");
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+		"premake5.lua"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
