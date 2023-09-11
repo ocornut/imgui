c1b928e19f3b01f9564b905b9895d09aea92a5b6
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2023-09-11 17:19:28 +0200

    Remove comment referencing removed field SortSign
    The 'SortSign' field was removed in commit ae6fc48f6048, but this
    comment still refers to this field.

--- imgui.h
+++ imgui.h
@@ -2229,7 +2229,7 @@ struct ImGuiTableColumnSortSpecs
     ImGuiID                     ColumnUserID;       // User id of the column (if specified by a TableSetupColumn() call)
     ImS16                       ColumnIndex;        // Index of the column
     ImS16                       SortOrder;          // Index within parent ImGuiTableSortSpecs (always stored in order starting from 0, tables sorted on a single criteria will always have a 0 here)
-    ImGuiSortDirection          SortDirection : 8;  // ImGuiSortDirection_Ascending or ImGuiSortDirection_Descending (you can use this or SortSign, whichever is more convenient for your sort function)
+    ImGuiSortDirection          SortDirection : 8;  // ImGuiSortDirection_Ascending or ImGuiSortDirection_Descending
 
     ImGuiTableColumnSortSpecs() { memset(this, 0, sizeof(*this)); }
 };
