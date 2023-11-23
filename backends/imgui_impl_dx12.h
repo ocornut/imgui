55946ad285072b89ca497fe1633556563c5b062e
Marc Delorme <delormemarco@gmail.com>
2023-11-23 19:12:33 +0900

    Convert DX12 backend to use explicit context API
    --
    This commit is part of the conversion of Dear ImGui toward explicit context API.
    This commit is supposed to be applied *after* make_explicit_imgui.py has been run.
    --

--- backends/imgui_impl_dx12.h
+++ backends/imgui_impl_dx12.h
@@ -31,14 +31,14 @@ struct D3D12_GPU_DESCRIPTOR_HANDLE;
 // Before calling the render function, caller must prepare cmd_list by resetting it and setting the appropriate
 // render target and descriptor heap that contains font_srv_cpu_desc_handle/font_srv_gpu_desc_handle.
 // font_srv_cpu_desc_handle and font_srv_gpu_desc_handle are handles to a single SRV descriptor to use for the internal font texture.
-IMGUI_IMPL_API bool     ImGui_ImplDX12_Init(ID3D12Device* device, int num_frames_in_flight, DXGI_FORMAT rtv_format, ID3D12DescriptorHeap* cbv_srv_heap,
+IMGUI_IMPL_API bool     ImGui_ImplDX12_Init(ImGuiContext* ctx, ID3D12Device* device, int num_frames_in_flight, DXGI_FORMAT rtv_format, ID3D12DescriptorHeap* cbv_srv_heap,
                                             D3D12_CPU_DESCRIPTOR_HANDLE font_srv_cpu_desc_handle, D3D12_GPU_DESCRIPTOR_HANDLE font_srv_gpu_desc_handle);
-IMGUI_IMPL_API void     ImGui_ImplDX12_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX12_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX12_RenderDrawData(ImDrawData* draw_data, ID3D12GraphicsCommandList* graphics_command_list);
+IMGUI_IMPL_API void     ImGui_ImplDX12_NewFrame(ImGuiContext* ctx);
+IMGUI_IMPL_API void     ImGui_ImplDX12_Shutdown(ImGuiContext* ctx);
+IMGUI_IMPL_API void     ImGui_ImplDX12_RenderDrawData(ImGuiContext* ctx, ImDrawData* draw_data, ID3D12GraphicsCommandList* graphics_command_list);
 
 // Use if you want to reset your rendering device without losing Dear ImGui state.
-IMGUI_IMPL_API void     ImGui_ImplDX12_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_ImplDX12_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplDX12_InvalidateDeviceObjects(ImGuiContext* ctx);
+IMGUI_IMPL_API bool     ImGui_ImplDX12_CreateDeviceObjects(ImGuiContext* ctx);
 
 #endif // #ifndef IMGUI_DISABLE
