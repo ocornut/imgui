d431d85839b507a9c9041b0e28de7c2443976292
ocornut <omarcornut@gmail.com>
2024-01-17 15:33:12 +0100

    Internals: removed obsolete ImPool::GetSize() (last used by implot 0.10, changed in implot 0.11)

--- imgui_internal.h
+++ imgui_internal.h
@@ -690,9 +690,6 @@ struct ImPool
     int         GetBufSize() const                  { return Buf.Size; }
     int         GetMapSize() const                  { return Map.Data.Size; }   // It is the map we need iterate to find valid items, since we don't have "alive" storage anywhere
     T*          TryGetMapData(ImPoolIdx n)          { int idx = Map.Data[n].val_i; if (idx == -1) return NULL; return GetByIndex(idx); }
-#ifndef IMGUI_DISABLE_OBSOLETE_FUNCTIONS
-    int         GetSize()                           { return GetMapSize(); } // For ImPlot: should use GetMapSize() from (IMGUI_VERSION_NUM >= 18304)
-#endif
 };
 
 // Helper: ImChunkStream<>
