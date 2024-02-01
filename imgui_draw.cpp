e97cb50b825358ad13c9120d88ada50d3127231c
Bruce Mitchener <bruce.mitchener@gmail.com>
2024-02-01 22:22:54 +0700

    Fix some typos.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -641,7 +641,7 @@ void ImDrawList::PrimReserve(int idx_count, int vtx_count)
     _IdxWritePtr = IdxBuffer.Data + idx_buffer_old_size;
 }
 
-// Release the a number of reserved vertices/indices from the end of the last reservation made with PrimReserve().
+// Release the number of reserved vertices/indices from the end of the last reservation made with PrimReserve().
 void ImDrawList::PrimUnreserve(int idx_count, int vtx_count)
 {
     IM_ASSERT_PARANOID(idx_count >= 0 && vtx_count >= 0);
