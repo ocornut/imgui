676474c5ad328bfb75ce593d83e02d57d47b7f74
Vladyslav Lytosh <vllitosh46@gmail.com>
2024-03-27 19:41:56 +0200

    Add vs studio files to gitignore, modify premake

--- premake5.lua
+++ premake5.lua
@@ -16,7 +16,8 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "imgui_tables.cpp"
 	}
 
 	filter "system:windows"
