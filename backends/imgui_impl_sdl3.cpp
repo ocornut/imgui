a4e3f3d7ce0109de2932ac6ec02073390679f943
Daniil Romanov <33653080+ranmasao@users.noreply.github.com>
2024-09-19 16:47:28 +0300

    Update imgui_impl_sdl3.cpp

--- backends/imgui_impl_sdl3.cpp
+++ backends/imgui_impl_sdl3.cpp
@@ -564,7 +564,7 @@ static void ImGui_ImplSDL3_UpdateMouseData()
     // We forward mouse input when hovered or captured (via SDL_EVENT_MOUSE_MOTION) or when focused (below)
 #if SDL_HAS_CAPTURE_AND_GLOBAL_MOUSE
     // SDL_CaptureMouse() let the OS know e.g. that our imgui drag outside the SDL window boundaries shouldn't e.g. trigger other operations outside
-    SDL_CaptureMouse((bd->MouseButtonsDown != 0) ? SDL_TRUE : SDL_FALSE);
+    SDL_CaptureMouse(bd->MouseButtonsDown != 0);
     SDL_Window* focused_window = SDL_GetKeyboardFocus();
     const bool is_app_focused = (bd->Window == focused_window);
 #else
