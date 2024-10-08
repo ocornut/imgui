f3d242a90d645dec043d63d128015c87f0ac9fe8
ocornut <omarcornut@gmail.com>
2024-10-08 19:45:58 +0200

    Tables: fixed initial auto-sizing issue with synched-instances. (#8045, #7218)

--- imgui.h
+++ imgui.h
@@ -29,7 +29,7 @@
 // Library Version
 // (Integer encoded as XYYZZ for use in #if preprocessor conditionals, e.g. '#if IMGUI_VERSION_NUM >= 12345')
 #define IMGUI_VERSION       "1.91.4 WIP"
-#define IMGUI_VERSION_NUM   19131
+#define IMGUI_VERSION_NUM   19132
 #define IMGUI_HAS_TABLE
 
 /*
