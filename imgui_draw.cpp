953ce90d27d361d00b4e299aa4aa62fa49cfebf4
ocornut <omar@miracleworld.net>
2025-01-13 20:25:16 +0100

    Fonts: ImFontAtlasBuildInit() uses the occasion to sync HasTexUpdates from imgui context, narrowing the scope where it isn't set.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -3912,6 +3912,8 @@ void ImFontAtlasBuildInit(ImFontAtlas* atlas)
     if (builder_is_new)
         builder = atlas->Builder = IM_NEW(ImFontAtlasBuilder)();
 
+    ImFontAtlasBuildUpdateRendererHasTexUpdatesFromContext(atlas);
+
     ImFontAtlasPackInit(atlas);
 
     // Add required texture data
