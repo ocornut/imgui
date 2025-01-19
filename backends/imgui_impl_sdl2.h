035799c5cc7357c206cf1e776f955db768573494
Daniel K. O. (dkosmari) <none@none>
2025-01-19 02:52:38 -0300

    Backends/SDL2: do not access nor manage the SDL_GameController objects directly, use events instead; removed ImGui_ImplSDL2_SetGamepadMode().
    --HG--
    branch : backend-sdl2-gamecontroller-events-hg

--- backends/imgui_impl_sdl2.h
+++ backends/imgui_impl_sdl2.h
@@ -38,9 +38,4 @@ IMGUI_IMPL_API void     ImGui_ImplSDL2_Shutdown();
 IMGUI_IMPL_API void     ImGui_ImplSDL2_NewFrame();
 IMGUI_IMPL_API bool     ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event);
 
-// Gamepad selection automatically starts in AutoFirst mode, picking first available SDL_Gamepad. You may override this.
-// When using manual mode, caller is responsible for opening/closing gamepad.
-enum ImGui_ImplSDL2_GamepadMode { ImGui_ImplSDL2_GamepadMode_AutoFirst, ImGui_ImplSDL2_GamepadMode_AutoAll, ImGui_ImplSDL2_GamepadMode_Manual };
-IMGUI_IMPL_API void     ImGui_ImplSDL2_SetGamepadMode(ImGui_ImplSDL2_GamepadMode mode, struct _SDL_GameController** manual_gamepads_array = nullptr, int manual_gamepads_count = -1);
-
 #endif // #ifndef IMGUI_DISABLE
