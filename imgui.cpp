0b71339122fabecf925a0a9fb8ca90dacaac71f9
ocornut <omar@miracleworld.net>
2025-03-05 17:05:24 +0100

    Demo: Add a "Fonts" section for visibility.

--- imgui.cpp
+++ imgui.cpp
@@ -15640,6 +15640,11 @@ void ImGui::ShowFontAtlas(ImFontAtlas* atlas)
 {
     ImGuiContext& g = *GImGui;
 
+    SeparatorText("Backend Support for Dynamic Fonts");
+    BeginDisabled();
+    CheckboxFlags("io.BackendFlags: RendererHasTextures", &GetIO().BackendFlags, ImGuiBackendFlags_RendererHasTextures);
+    EndDisabled();
+
     SeparatorText("Fonts");
     Text("Read ");
     SameLine(0, 0);
@@ -16548,12 +16553,11 @@ void ImGui::DebugNodeFont(ImFont* font)
     }
     if (SmallButton("Set as default"))
         GetIO().FontDefault = font;
-    if (atlas->Fonts.Size > 1 && !atlas->Locked)
-    {
-        SameLine();
-        if (SmallButton("Remove"))
-            atlas->RemoveFont(font);
-    }
+    SameLine();
+    BeginDisabled(atlas->Fonts.Size <= 1 || atlas->Locked);
+    if (SmallButton("Remove"))
+        atlas->RemoveFont(font);
+    EndDisabled();
 
     // Display details
     SetNextItemWidth(GetFontSize() * 8);
