e8b6ec7fc183cb95b48154128269b3431d822571
ocornut <omar@miracleworld.net>
2025-03-06 18:15:25 +0100

    WIP - Obsolete GetGlyphRangesXXX() functions.

--- imgui_draw.cpp
+++ imgui_draw.cpp
@@ -4586,6 +4586,7 @@ const ImFontLoader* ImFontAtlasGetFontLoaderForStbTruetype()
 // [SECTION] ImFontAtlas: glyph ranges helpers
 //-------------------------------------------------------------------------
 // - GetGlyphRangesDefault()
+// Obsolete functions since 1.92:
 // - GetGlyphRangesGreek()
 // - GetGlyphRangesKorean()
 // - GetGlyphRangesChineseFull()
@@ -4607,6 +4608,7 @@ const ImWchar*   ImFontAtlas::GetGlyphRangesDefault()
     return &ranges[0];
 }
 
+#ifndef IMGUI_DISABLE_OBSOLETE_FUNCTIONS
 const ImWchar*   ImFontAtlas::GetGlyphRangesGreek()
 {
     static const ImWchar ranges[] =
@@ -4856,6 +4858,7 @@ const ImWchar*  ImFontAtlas::GetGlyphRangesVietnamese()
     };
     return &ranges[0];
 }
+#endif // #ifndef IMGUI_DISABLE_OBSOLETE_FUNCTIONS
 
 //-----------------------------------------------------------------------------
 // [SECTION] ImFontGlyphRangesBuilder
