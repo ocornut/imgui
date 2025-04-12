d3bb3336f5359dd3bf4af9bb32c2164b31414eac
519q <conspir8ed@gmail.com>
2025-04-12 17:54:10 +0300

    Backends: OSX: remove duplicate variable. (#8565)

--- backends/imgui_impl_osx.mm
+++ backends/imgui_impl_osx.mm
@@ -788,8 +788,6 @@ static bool ImGui_ImplOSX_HandleEvent(NSEvent* event, NSView* view)
                 default:
                     return io.WantCaptureKeyboard;
             }
-
-            NSEventModifierFlags modifier_flags = [event modifierFlags];
             io.AddKeyEvent(key, (modifier_flags & mask) != 0);
             io.SetKeyEventNativeData(key, key_code, -1); // To support legacy indexing (<1.87 user code)
         }
