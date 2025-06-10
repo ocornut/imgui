fff831e1502f3e46094ac429514dce0240518aaa
FJdarc <1204551784@qq.com>
2025-06-10 19:44:41 +0800

    新增 ImGui 项目的 Premake5 配置文件

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+  targetdir ("build/" .. outputdir .. "/bin")
+  objdir ("build/" .. outputdir .. "/obj/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
