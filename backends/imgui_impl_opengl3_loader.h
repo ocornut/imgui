23b790e186d96758b24d939c33a8e58a1ff6fdd1
Tim-Rex <tim.kane@gmail.com>
2025-07-12 16:49:57 +1000

    Update imgui_impl_opengl3_loader.h
    Close libgl on shutdown

--- backends/imgui_impl_opengl3_loader.h
+++ backends/imgui_impl_opengl3_loader.h
@@ -476,6 +476,7 @@ typedef GL3WglProc (*GL3WGetProcAddressProc)(const char *proc);
 /* gl3w api */
 GL3W_API int imgl3wInit(void);
 GL3W_API int imgl3wInit2(GL3WGetProcAddressProc proc);
+GL3W_API void imgl3wShutdown(void);
 GL3W_API int imgl3wIsSupported(int major, int minor);
 GL3W_API GL3WglProc imgl3wGetProcAddress(const char *proc);
 
@@ -831,6 +832,11 @@ int imgl3wInit2(GL3WGetProcAddressProc proc)
 {
     load_procs(proc);
     return parse_version();
+}
+
+void imgl3wShutdown(void)
+{
+    close_libgl();
 }
 
 int imgl3wIsSupported(int major, int minor)
