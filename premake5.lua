0b433d3bf7d7da7e57f4b91be72b9745fa7b8de4
Ramoj naidu <143724619+Ramojinaidu@users.noreply.github.com>
2025-12-16 15:54:05 +0530

    premake support for imgui

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/%{prj.name}")
+	objdir ("bin-int/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp",
+	}
+	
+	includedirs
+	{
+		"*.h"
+	}
+
+
+	filter "system:linux"
+		pic "Default"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
