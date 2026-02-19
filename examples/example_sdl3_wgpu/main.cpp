eb85c65f7c952c74f936c8f0986e72c123618b3a
Adem Budak <adembudak@outlook.com>
2026-02-19 13:27:19 +0300

    Examples: SDL2+WGPU add missing static keyword on function declaration

--- examples/example_sdl3_wgpu/main.cpp
+++ examples/example_sdl3_wgpu/main.cpp
@@ -41,7 +41,7 @@ static int                      wgpu_surface_height = 800;
 
 // Forward declarations
 static bool InitWGPU(SDL_Window* window);
-WGPUSurface CreateWGPUSurface(const WGPUInstance& instance, SDL_Window* window);
+static WGPUSurface CreateWGPUSurface(const WGPUInstance& instance, SDL_Window* window);
 
 static void ResizeSurface(int width, int height)
 {
