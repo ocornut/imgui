f849e95b83f84fb0e7b155a93d1710daa4cdef5b
Alexander "FireFox" Ong <FireFox2000000@gmail.com>
2026-04-28 20:44:20 +1000

    Fixed InputScalar not parsing user input when the display format is configured not to show the scalar value. Useful for displaying "mixed" inputs, where a single field might represent multiple different values.
    # Conflicts:
    #	imgui_widgets.cpp

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -2378,9 +2378,17 @@ bool ImGui::DataTypeApplyFromText(const char* buf, ImGuiDataType data_type, void
     // - In theory could treat empty format as using default, but this would only cover rare/bizarre case of using InputScalar() + integer + format string without %.
     char format_sanitized[32];
     if (data_type == ImGuiDataType_Float || data_type == ImGuiDataType_Double)
+    {
         format = type_info->ScanFmt;
+    }
     else
+    {
         format = ImParseFormatSanitizeForScanning(format, format_sanitized, IM_COUNTOF(format_sanitized));
+
+        // Format doesn't want us to show the number currently, but we still need to parse the resulting input
+        if (format && format[0] == '\0')
+            format = type_info->ScanFmt;
+    }
 
     // Small types need a 32-bit buffer to receive the result from scanf()
     int v32 = 0;
