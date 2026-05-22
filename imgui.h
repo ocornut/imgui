d0a2388f919d605a35583f10e1f6a9a1960fa0a6
ocornut <omar@miracleworld.net>
2026-05-22 20:36:19 +0200

    DrawList: Textured Round Corners: use same array for fill and strokes.
    Feels like a little less book-keeping.

--- imgui.h
+++ imgui.h
@@ -3320,15 +3320,15 @@ struct ImGuiSelectionExternalStorage
 
 // The maximum line width to bake anti-aliased textures for. Build atlas with ImFontAtlasFlags_NoBakedLines to disable baking.
 #ifndef IM_DRAWLIST_TEX_LINES_WIDTH_MAX
-#define IM_DRAWLIST_TEX_LINES_WIDTH_MAX     (32)
-#define IM_DRAWLIST_TEX_LINE_FRACT_WIDTH_MAX     (128)
+#define IM_DRAWLIST_TEX_LINES_WIDTH_MAX         (32)
+#define IM_DRAWLIST_TEX_LINE_FRACT_WIDTH_MAX    (128)
 #endif
 
 #ifndef IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX
-#define IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX     (16)
+#define IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX    (16)
 #endif
 #ifndef IM_DRAWLIST_TEX_CORNERS_THICKNESS_MAX
-#define IM_DRAWLIST_TEX_CORNERS_THICKNESS_MAX     (3)
+#define IM_DRAWLIST_TEX_CORNERS_THICKNESS_MAX   (4)     // 0: fill, 1-3: strokes thickness
 #endif
 
 // ImDrawIdx: vertex index. [Compile-time configurable type]
@@ -4017,8 +4017,7 @@ struct ImFontAtlas
     ImVector<ImFontConfig>      Sources;            // Source/configuration data
     ImVec4                      TexUvLines[IM_DRAWLIST_TEX_LINES_WIDTH_MAX + 1];        // UVs for baked anti-aliased lines
     ImVec4                      TexUvLineFract;                                         // UVs for fraction thickness baked anti-aliased lines
-    ImVec4                      TexUvCornerFills[IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX]; // UVs for baked anti-aliased corners
-    ImVec4                      TexUvCornerStrokes[IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX * IM_DRAWLIST_TEX_CORNERS_THICKNESS_MAX];  // UVs for baked anti-aliased corners
+    ImVec4                      TexUvCorners[IM_DRAWLIST_TEX_CORNERS_ROUNDING_MAX * IM_DRAWLIST_TEX_CORNERS_THICKNESS_MAX];  // UVs for baked anti-aliased corners (0= fill, 1> stroke thickness)
     int                         TexNextUniqueID;    // Next value to be stored in TexData->UniqueID
     int                         FontNextUniqueID;   // Next value to be stored in ImFont->FontID
     ImVector<ImDrawListSharedData*> DrawListSharedDatas; // List of users for this atlas. Typically one per Dear ImGui context.
