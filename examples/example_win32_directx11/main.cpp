c56adb44008c0a063346eb1a1628b9f1282858c2
amer <amer.software1@gmail.com>
2026-06-04 18:48:40 +0300

    commit 2

--- examples/example_win32_directx11/main.cpp
+++ examples/example_win32_directx11/main.cpp
@@ -105,7 +105,7 @@ int main(int, char**)
     //IM_ASSERT(font != nullptr);
 
     // Our state
-    bool show_demo_window = true;
+    bool show_demo_window = false;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
