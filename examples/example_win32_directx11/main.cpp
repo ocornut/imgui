e313724109ace86415baef09597389a261316640
amer <amer.software1@gmail.com>
2026-06-05 08:19:35 +0300

    New Window added

--- examples/example_win32_directx11/main.cpp
+++ examples/example_win32_directx11/main.cpp
@@ -148,6 +148,11 @@ int main(int, char**)
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
+
+        ImGui::Begin("new branch window ");
+
+        ImGui::End();
+
         // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
