f1cc2ae15e53a861a874c3034aae6798fde194ab
ocornut <omar@miracleworld.net>
2026-07-31 15:51:26 +0200

    Version 1.92.9b

--- imgui.h
+++ imgui.h
@@ -1,4 +1,4 @@
-// dear imgui, v1.92.9
+// dear imgui, v1.92.9b
 // (headers)
 
 // Help:
@@ -29,8 +29,8 @@
 
 // Library Version
 // (Integer encoded as XYYZZ for use in #if preprocessor conditionals, e.g. '#if IMGUI_VERSION_NUM >= 12345')
-#define IMGUI_VERSION       "1.92.9"
-#define IMGUI_VERSION_NUM   19290
+#define IMGUI_VERSION       "1.92.9b"
+#define IMGUI_VERSION_NUM   19291
 #define IMGUI_HAS_TABLE             // Added BeginTable() - from IMGUI_VERSION_NUM >= 18000
 #define IMGUI_HAS_TEXTURES          // Added ImGuiBackendFlags_RendererHasTextures - from IMGUI_VERSION_NUM >= 19198
 
--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -1,4 +1,4 @@
-// dear imgui, v1.92.9
+// dear imgui, v1.92.9b
 // (widgets code)
 
 /*
